bde3a2b4ad47fc9ad5695ecb04f2916b235cccfd
unknown <hitprofit70@gmail.com>
2022-08-02 08:41:08 +0200

    Javascrpit updated

---
+++ login.js
@@ -0,0 +1,22 @@
+// var username;
+// var password;
+
+
+// function Validlogin(value) {
+//     if (!value) { false
+//         return
+//     } true
+// }
+
+// function inputTwousers() {
+//     username = document.getElementById('username').value
+//     password = document.getElementById('password').value
+// }
+
+function addSubmit() {
+    console.log('Login form created by Sibusiso');
+}
+
+// function displayonScreen() {}
+
+// displayonScreen();
